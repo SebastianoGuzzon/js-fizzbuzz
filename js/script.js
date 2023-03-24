9e3ea157fc9c9f1a809f4c1c087cc1930c91a777
SebastianoGuzzon <sebyguzz@icloud.com>
2023-03-24 10:03:01 +0100

    Final Commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,45 @@
+// for (var i = 1; i <= 100; i++) {
+//   if ((i%5 == 0)&&(i%3 == 0)) {
+//     console.log("fizzbuzz");
+//     document.getElementById("lista").innerHTML += "fizzbuzz" + "<br>";
+//   }
+//   else if (i%3 == 0) {
+//     console.log("fizz");
+//     document.getElementById("lista").innerHTML += "fizz" + "<br>";
+//   }
+//   else if (i%5 == 0) {
+//     console.log("buzz");
+//     document.getElementById("lista").innerHTML += "buzz" + "<br>";
+//   }
+//   else {
+//     console.log(i);
+//     document.getElementById("lista").innerHTML += i + "<br>";
+//   }
 
-const container = document.querySelector(".container");
-  console.log(container)
-  
-for (let i = 1; i <= 100; i++){
-    console.log(i)
+// }
 
-    const box = document.createElement("div")
+const container = document.querySelector('.container');
 
-    box.classList.add("box")
+for(let i = 1; i <= 100; i++){
 
-    box.append(i)
-  
-    container.append(box)
+  const box = document.createElement('div');
+  box.className = 'box';
+
+  let output = i;
+
+  if(!(i % 15)){
+    output = 'FizzBuzz';
+    box.classList.add('fizzbuzz');
+  }else if(!(i % 3)){
+    output = 'Fizz'
+    box.classList.add('fizz');
+  }else if(!(i % 5)){
+    output = 'Buzz'
+    box.classList.add('buzz');
   }
-  
+
+  box.append(output);
+
+  container.append(box);
+
+}
